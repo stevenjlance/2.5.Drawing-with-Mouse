dd23491c83c4520e2df57cca063ab20d2073166f
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-03-01 13:21:56 +0000

    Solutions added

--- script.js
+++ script.js
@@ -5,12 +5,68 @@ function setup() {
   background(200);
 }
 function draw() {
-background(200);
- //1. Uncomment the line below this and hit run. What to do you see? Delete line 7. What do you see? Why do you think that is?
- //text(frameCount, 20,20)
-
+	// background(200);
+	//1. Uncomment the line below this and hit run. What to do you see? Delete line 7. What do you see? Why do you think that is?
+	//text(frameCount, 20,20)
+	// noStroke()
 
 	// Code the rest of your code below this line!
+
+	// Challenge #1:
+	// if(mouseX > width/2){
+	// 	fill("red")
+	// }
+	// else{
+	// 	fill("blue")
+	// }
+	// ellipse(mouseX, mouseY, 20, 20)
+
+	// Challenge #2:
+	// if(mouseX > 400 && mouseX < 500){
+	// 	fill("yellow")
+	// }
+	// else if(mouseX > 200 && mouseX < 400){
+	// 	fill("mistyrose")
+	// }
+	// else {
+	// 	fill("blue")
+	// }
+	// ellipse(mouseX, mouseY, 20, 20)
+
+	// Challenge #3:
+	// fill(mouseX, mouseY, 0)
+	// ellipse(mouseX, mouseY, 20, 20)
+
+	// Challenge 4:
+	// if(mouseX < 100 || mouseX > 500){
+	// 	stroke("blue")
+	// }
+	// else if(mouseX > 100 && mouseX < 250){
+	// 	stroke("green")
+	// }
+	// else {
+	// 	stroke("purple")
+	// }
+	// strokeWeight(10)
+	// line(mouseX, mouseY, pmouseX, pmouseY)
+
+	// Challenge 5:
+	// if(mouseX < 0){
+	// 	stroke("blue")
+	// }
+	// else if(mouseX > width){
+	// 	stroke("green")
+	// }
+	// else if(mouseY < 0){
+	// 	stroke("purple")
+	// }
+	// else if(mouseY > height){
+	// 	stroke("red")
+	// }
+	// strokeWeight(10)
+	// line(mouseX, mouseY, pmouseX, pmouseY)
+
+	
 	
 
 
